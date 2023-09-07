a2b9e8a02f9a824d4084fb5992c59f407a6ed064
muhafrom <muhafrom@gmail.com>
2023-09-07 07:42:03 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A c program to print qoute using write function
+ * Return: Alawys 1 (not success)
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
