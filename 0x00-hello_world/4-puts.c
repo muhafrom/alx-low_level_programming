7df25c3822d9ce97d95e24abce488f999fead9f8
muhafrom <muhafrom@gmail.com>
2023-09-07 07:10:52 -0700

    Edit on c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-main - entry poiny
-description - puts to print a string
+ * main - Entry point
+ * Description: A c program that uses puts to print a string
+ * Return: Alawys 0 (Success)
 */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
