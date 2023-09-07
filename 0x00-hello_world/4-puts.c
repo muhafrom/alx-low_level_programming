0b38a52893b90c30ee9d18827469bdc86812cb28
muhafrom <muhafrom@gmail.com>
2023-09-07 07:05:50 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+main - entry poiny
+description - puts to print a string
+*/
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
