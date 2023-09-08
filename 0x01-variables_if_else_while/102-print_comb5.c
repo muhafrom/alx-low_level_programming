d88f752d28ca5d7fdcca28ea90061c2eb04728c9
muhafrom <muhafrom@gmail.com>
2023-09-08 10:25:57 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,25 +10,15 @@ int i;
 int j;
 int k;
 int l;
-for (i = 0; i <= 9; ++i)
-{
-for (j = 0; j <= 9; ++j)
-{
-for (k = 0; k <= 9; ++k)
-{
-for (l = 0; l <= 9; ++l)
-{
-putchar(i + '0');
-putchar(j + '0');
+for (i = 0; i <= 99; i++) {
+for (j = i; j <= 99; j++) {
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-if (i == j && j == k && k == l && l == 9)
-break;
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 return (0);
