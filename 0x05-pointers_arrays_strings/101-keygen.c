14537786e863f0c4a9e2cd4ce8273d2e0d9febf0
muhafrom <muhafrom@gmail.com>
2023-09-18 10:20:38 -0700

    pass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - generates random valid passwords
+ * main - generates random valid passwords 101-crackme
  * Return: Always 0.
  */
 int main(void)
