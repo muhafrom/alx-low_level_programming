baa7b3b289546ba10a91c3337c343823b9458e8b
muhafrom <muhafrom@gmail.com>
2023-09-21 05:28:06 -0700

    m

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+int i, j;
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i]; i++)
+{
+for (j = 0; alphabet[j]; j++)
+{
+if (s[i] == alphabet[j])
+{
+s[i] = rot13[j];
+break;
+}
+}
+}
+return s;
+}
