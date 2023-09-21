8086172328e6abdd1f7e02b0c05c0feade74fb8a
muhafrom <muhafrom@gmail.com>
2023-09-21 04:58:34 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,6 @@
 char *cap_string(char *s)
 {
 int i = 0;
-int j = 0;
 int new_word = 1;
 char separators[] = " \t\n,;.!?\"(){}";
 while (s[i])
@@ -17,6 +16,7 @@ if (new_word && s[i] >= 'a' && s[i] <= 'z')
 s[i] = s[i] - 'a' + 'A';
 new_word = 0;
 }
+int j = 0;
 for (j = 0; separators[j]; j++)
 {
 if (s[i] == separators[j])
