f87e8d72ddfae39908bb536301586df7cbb96d29
muhafrom <muhafrom@gmail.com>
2023-09-21 04:46:24 -0700

    ad

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be converted
+ * Return: pointer to the converted string
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+int new_word = 1;
+char separators[] = " \t\n,;.!?\"(){}";
+while (s[i])
+{
+if (new_word && s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 'a' + 'A';
+new_word = 0;
+}
+for (int j = 0; separators[j]; j++)
+{
+if (s[i] == separators[j])
+{
+new_word = 1;
+break;
+}
+}
+i++;
+}
+return (s);
+}
