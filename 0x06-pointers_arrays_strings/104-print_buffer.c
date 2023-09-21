791b637c8cc2f004b24d0500505f0157181c005e
muhafrom <muhafrom@gmail.com>
2023-09-21 06:52:21 -0700

    104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,82 +1,71 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * printable - declares whether a is a printable ASCII char
- * @n: intger
- * Return: 1 if true 0 if false
- */
-int printable(int n)
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+*/
+int isPrintableASCII(int n)
 {
-return (n >= "32" && n <= "126");
+return (n >= 32 && n <= 126);
 }
 /**
- * hex - print hex value for string b in formatted form
+ * printHexes - print hex values for string b in formatted form
  * @b: string to print
- * @start: starting point
- * @end: end point
- */
-void hex(char *b, int start, int end)
+ * @start: starting position
+ * @end: ending position
+*/
+void printHexes(char *b, int start, int end)
 {
 int i = 0;
 while (i < 10)
 {
 if (i < end)
-{
 printf("%02x", *(b + start + i));
-}
 else
-{
 printf("  ");
-}
 if (i % 2)
-{
 printf(" ");
-}
 i++;
 }
 }
 /**
- * asc - print ASCII value for string b in formatted form
- * to replace nonprintable char with '*'
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
  * @b: string to print
- * @start: starting point
- * @end: end point
- */
-void hex(char *b, int start, int end)
+ * @start: starting position
+ * @end: ending position
+*/
+void printASCII(char *b, int start, int end)
 {
-int i = 0, ch;
+int ch, i = 0;
 while (i < end)
 {
 ch = *(b + i + start);
-if (!printable(ch))
-{
+if (!isPrintableASCII(ch))
 ch = 46;
-}
 printf("%c", ch);
 i++;
 }
 }
 /**
- * print_buffer - Prints the content of a buffer
- * @b: The buffer to print
- * @size: The number of bytes to print
- */
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+*/
 void print_buffer(char *b, int size)
 {
 int start, end;
 if (size > 0)
 {
-for (start = 0 ; start < size; start += 10)
+for (start = 0; start < size; start += 10)
 {
 end = (size - start < 10) ? size - start : 10;
 printf("%08x: ", start);
-hex(b, start, end);
-asc(b, start, end);
+printHexes(b, start, end);
+printASCII(b, start, end);
 printf("\n");
 }
-}
-else
-{
+} else
 printf("\n");
-}
 }
