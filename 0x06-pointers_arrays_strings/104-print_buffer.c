a7f9cb96a9085d6837acd8da53089b95a958726c
muhafrom <muhafrom@gmail.com>
2023-09-21 06:46:37 -0700

    104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,82 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * printable - declares whether a is a printable ASCII char
+ * @n: intger
+ * Return: 1 if true 0 if false
+ */
+int printable(int n)
+{
+return (n >= "32" && n <= "126");
+}
+/**
+ * hex - print hex value for string b in formatted form
+ * @b: string to print
+ * @start: starting point
+ * @end: end point
+ */
+void hex(char *b, int start, int end)
+{
+int i = 0;
+while (i < 10)
+{
+if (i < end)
+{
+printf("%02x", *(b + start + i));
+}
+else
+{
+printf("  ");
+}
+if (i % 2)
+{
+printf(" ");
+}
+i++;
+}
+}
+/**
+ * asc - print ASCII value for string b in formatted form
+ * to replace nonprintable char with '*'
+ * @b: string to print
+ * @start: starting point
+ * @end: end point
+ */
+void hex(char *b, int start, int end)
+{
+int i = 0, ch;
+while (i < end)
+{
+ch = *(b + i + start);
+if (!printable(ch))
+{
+ch = 46;
+}
+printf("%c", ch);
+i++;
+}
+}
+/**
  * print_buffer - Prints the content of a buffer
  * @b: The buffer to print
  * @size: The number of bytes to print
  */
 void print_buffer(char *b, int size)
 {
-int i, j;
-if (size <= 0)
+int start, end;
+if (size > 0)
 {
+for (start = 0 ; start < size; start += 10)
+{
+end = (size - start < 10) ? size - start : 10;
+printf("%08x: ", start);
+hex(b, start, end);
+asc(b, start, end);
 printf("\n");
-return;
 }
-for (i = 0; i < size; i += 10)
-{
-printf("%08x: ", i);
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-printf("%02x", b[i + j]);
-else
-printf("  ");
-if (j % 2 == 1)
-printf(" ");
-}
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-if (b[i + j] >= 32 && b[i + j] <= 126)
-printf("%c", b[i + j]);
-else
-printf(".");
 }
 else
 {
-printf(" ");
-}
-}
 printf("\n");
 }
 }
