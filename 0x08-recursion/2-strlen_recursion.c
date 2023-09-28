91be8dde0dcc7e7ee6c854d6e3312135716288f6
muhafrom <muhafrom@gmail.com>
2023-09-28 09:38:28 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate length of
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+else
+{
+return (0);
+}
+}
